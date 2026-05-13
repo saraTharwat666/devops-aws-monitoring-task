9eb34e0335c4b0ebce0cc9d5aac651d3340c6644
sara Tharwat <sarahtharwat666@gmail.com>
2026-05-13 11:27:55 +0300

    Updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,8 @@
 resource "aws_instance" "main" {
-  ami           = "ami-0c02fb55956c7d316" # Ubuntu 22.04 us-east-1
+  ami           = "ami-0c02fb55956c7d316"
   instance_type = var.instance_type
-
- key_name = aws_key_pair.devops_key.key_name
-
- user_data = file("${path.module}/cloud-init.sh")
+  key_name      = aws_key_pair.devops_key.key_name
+  user_data     = file("${path.module}/cloud-init.sh")
 
   vpc_security_group_ids = [aws_security_group.devops_sg.id]
 
